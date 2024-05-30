4e367fa856f8116da030af607aeec5020d0429a1
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2024-05-31 01:13:13 +0400

    Create script2.js

---
+++ pages/js/script2.js
@@ -0,0 +1,5 @@
+//navigation
+document.getElementById('menu-icon').addEventListener('click', function() {
+    document.getElementById('nav-left').classList.toggle('active');
+    document.getElementById('nav-right').classList.toggle('active');
+});
