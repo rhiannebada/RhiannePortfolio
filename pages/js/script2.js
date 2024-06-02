d7ee970ca5b334a93017cc4146a0cdfd1fb58778
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2024-06-02 10:43:20 +0400

    Update script2.js

--- pages/js/script2.js
+++ pages/js/script2.js
@@ -1,5 +1,8 @@
 //navigation
+// add an event listener to the element with id 'menu-icon' for the 'click' event
 document.getElementById('menu-icon').addEventListener('click', function() {
+    // toggle active class on the element with id nav-left
     document.getElementById('nav-left').classList.toggle('active');
+    // toggle active class on the element with id nav-left
     document.getElementById('nav-right').classList.toggle('active');
 });
