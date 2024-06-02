03e193c169b8d008879f4786dbe25d3418287aca
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2024-06-02 10:43:14 +0400

    Update script5.js

--- pages/js/script5.js
+++ pages/js/script5.js
@@ -1,29 +1,24 @@
-/*--------------------
-Vars
---------------------*/
+// works page
+// select menu element
 const $menu = document.querySelector('.menu');
+// select all menu items
 const $items = document.querySelectorAll('.menu--item');
 let menuHeight = $menu.clientHeight;
 let itemHeight = $items[0].clientHeight;
-let wrapHeight = $items.length * itemHeight;
+let wrapHeight = $items.length * itemHeight; // calcuate total height of all menu items
 
+// initial scroll speed and positions
 let scrollSpeed = 0;
 let oldScrollY = 0;
 let scrollY = 0;
 let y = 0;
 
-
-/*--------------------
-Lerp
---------------------*/
+// linear interpolation function to smooth out transitions
 const lerp = (v0, v1, t) => {
   return v0 * (1 - t) + v1 * t;
 };
 
-
-/*--------------------
-Dispose
---------------------*/
+// set position of menu items
 const dispose = scroll => {
   gsap.set($items, {
     y: i => {
@@ -31,26 +26,21 @@ const dispose = scroll => {
     },
     modifiers: {
       y: y => {
+        // wrap y value within heigh range to create infinite loop effect
         const s = gsap.utils.wrap(-itemHeight, wrapHeight - itemHeight, parseInt(y));
         return `${s}px`;
       } } });
 
-
+// initial call to position items
 };
 dispose(0);
 
-
-/*--------------------
-Wheel
---------------------*/
+// handle mouse wheel events to update scroll position
 const handleMouseWheel = e => {
   scrollY -= e.deltaY;
 };
 
-
-/*--------------------
-Touch
---------------------*/
+// variables for touch interactions
 let touchStart = 0;
 let touchY = 0;
 let isDragging = false;
@@ -59,59 +49,50 @@ const handleTouchStart = e => {
   isDragging = true;
   $menu.classList.add('is-dragging');
 };
+// touch move events
 const handleTouchMove = e => {
   if (!isDragging) return;
   touchY = e.clientY || e.touches[0].clientY;
   scrollY += (touchY - touchStart) * 2.5;
   touchStart = touchY;
 };
+// touch and events
 const handleTouchEnd = () => {
   isDragging = false;
   $menu.classList.remove('is-dragging');
 };
 
-
-/*--------------------
-Listeners
---------------------*/
+// add event listeners for mouse wheel and touch events
 $menu.addEventListener('mousewheel', handleMouseWheel);
-
 $menu.addEventListener('touchstart', handleTouchStart);
 $menu.addEventListener('touchmove', handleTouchMove);
 $menu.addEventListener('touchend', handleTouchEnd);
-
 $menu.addEventListener('mousedown', handleTouchStart);
 $menu.addEventListener('mousemove', handleTouchMove);
 $menu.addEventListener('mouseleave', handleTouchEnd);
 $menu.addEventListener('mouseup', handleTouchEnd);
+$menu.addEventListener('selectstart', () => {return false;}); // prevent text selection
 
-$menu.addEventListener('selectstart', () => {return false;});
-
-
-/*--------------------
-Resize
---------------------*/
+// window resize events to update dimensions
 window.addEventListener('resize', () => {
   menuHeight = $menu.clientHeight;
   itemHeight = $items[0].clientHeight;
   wrapHeight = $items.length * itemHeight;
 });
 
-
-/*--------------------
-Render
---------------------*/
-const render = () => {
-  requestAnimationFrame(render);
-  y = lerp(y, scrollY, .1);
+// main render loop
+const render = () => { // call render on the next frame
+  requestAnimationFrame(render); // smoothly interpolate y to scrollY
+  y = lerp(y, scrollY, .1); // update positions of menu items
   dispose(y);
 
-  scrollSpeed = y - oldScrollY;
-  oldScrollY = y;
+  scrollSpeed = y - oldScrollY; // calculate scroll speed
+  oldScrollY = y; // update old scroll position
 
+  // animate scale and rotation of menu items based on scroll speed
   gsap.to($items, {
     scale: 1 - Math.min(100, Math.abs(scrollSpeed)) * .005,
     rotate: scrollSpeed * 0.2 });
 
 };
-render();
+render(); // initial call to render
