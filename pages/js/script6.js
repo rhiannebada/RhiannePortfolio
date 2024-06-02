0c34d269e299279aa3bc1c7e26bed6de9a344739
Rhianne Ghernelle Intal Bada <badarhianne@gmail.com>
2024-06-02 10:43:12 +0400

    Update script6.js

--- pages/js/script6.js
+++ pages/js/script6.js
@@ -1,24 +1,29 @@
-//cursor
+// custom cursor
+// main cursor
 const cursor = document.createElement('div');
 cursor.classList.add('cursor');
 document.body.appendChild(cursor);
 
-//following cursor
+// following cursor element (smaller circle)
 const follower = document.createElement('div');
 follower.classList.add('cursor', 'cursor__follower');
 document.body.appendChild(follower);
 
+// add event listener to track mouse movements
 document.addEventListener('mousemove', (e) => {
   setPosition(follower, e);
   setPosition(cursor, e);
 });
 
+// function to set the position of cursor elements
 function setPosition(element, e) {
     const scrollX = window.pageXOffset || document.documentElement.scrollLeft;
     const scrollY = window.pageYOffset || document.documentElement.scrollTop;
 
+    // calculate mouse X and mouse Y position including scroll offset
     const mouseX = e.clientX + scrollX;
     const mouseY = e.clientY + scrollY;
 
+    // update element's transform style to move it to the mouse position
     element.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
 }
